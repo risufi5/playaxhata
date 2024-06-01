012d77f9729aa8fdd4524fc4eec72145f1618f54
renisisufi <renis.isufi5@gmail.com>
2024-06-01 17:58:06 +0200

    changes

--- app/Http/Controllers/CodeController.php
+++ app/Http/Controllers/CodeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Code;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
@@ -26,14 +27,18 @@ class CodeController extends Controller
            'code' => 'required|max:6|min:6',
        ]);
 
-        $codeExists = Code::where('value', $request->code)->first();
+        $code = Code::where('value', $request->code)->first();
 
-        if ($codeExists) {
-            $codeExists->update([
-                'start_used' => true,
-                'start_used_date' => date('Y-m-d H:i:s'),
-                'user_id' => $request->user()['id']
-            ]);
+        if ($code) {
+            if (($code->start_used && $code->finish_used) !== true) {
+                $code->update([
+                    'start_used' => true,
+                    'start_used_date' => date('Y-m-d H:i:s'),
+                    'user_id' => $request->user()['id']
+                ]);
+            } else {
+                return response()->json(['message' => 'Code expired!'], 200);
+            }
             return response()->json(['message' => 'You are in!'], 200);
         } else {
             return response()->json(['message' => 'Code is invalid'], 404);
@@ -51,12 +56,21 @@ class CodeController extends Controller
         $code = Code::where('value', $request->code)->first();
 
         if ($code) {
-            $code->update([
-                'score' => $request->score,
-                'finish_used' => true,
-                'finish_used_date' => date('Y-m-d H:i:s'),
-                'video_url' => $request->video_url
-            ]);
+            if (($code->start_used === true) && ($code->finish_used !== true)) {
+                $code->update([
+                    'score' => $request->score,
+                    'finish_used' => true,
+                    'finish_used_date' => date('Y-m-d H:i:s'),
+                    'video_url' => $request->video_url
+                ]);
+                $user = User::where('id', $request->user()['id'])->first();
+                $totalScore = $user->total_score;
+                $user->update([
+                    'total_score' => ((int)$totalScore + $request->score)
+                ]);
+            } else {
+                return response()->json(['message' => 'Game already played!'], 200);
+            }
             return response()->json(['message' => 'Thanks!'], 200);
         } else {
             return response()->json(['message' => 'Code is invalid'], 404);
